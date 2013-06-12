906035c6ad18352f4a34c150a73cf599893ec78d
Rob Bishop <rob@rob-bishop.com>
2013-06-12 09:42:17 +0100

    Update output files and change version string to 1.2beta from 1.1

--- recovery/config.h
+++ recovery/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 /* Version number displayed in the title bar */
-#define VERSION_NUMBER "1.1"
+#define VERSION_NUMBER "1.2beta"
 
 /* Color of the background */
 // #define BACKGROUND_COLOR  Qt::white
