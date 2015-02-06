fb972643653a70f5cab14cde1834d56dc666c284
Andrew Scheller <github@loowis.durge.org>
2015-02-06 15:34:40 +0000

    Use an explicit kernel version too (and record it in BUILD-DATA)

--- BUILDME.sh
+++ BUILDME.sh
@@ -48,6 +48,13 @@ function update_github_package_version {
 }
 
 
+function get_kernel_version {
+    CONFIG_FILE=.config
+    CONFIG_VAR=BR2_LINUX_KERNEL_VERSION
+    grep -E "^$CONFIG_VAR=\".+\"$" "$CONFIG_FILE" | tr -d '"' | cut -d= -f2
+}
+
+
 cd buildroot
 
 # WARNING: don't try changing these - you'll break buildroot
@@ -100,6 +107,7 @@ echo "NOOBS Version: $(git describe)" >> "$BUILD_INFO"
 echo "NOOBS Git HEAD @ $(git rev-parse --verify HEAD)" >> "$BUILD_INFO"
 echo "rpi-userland Git master @ $(get_package_version rpi-userland)" >> "$BUILD_INFO"
 echo "rpi-firmware Git master @ $(get_package_version rpi-firmware)" >> "$BUILD_INFO"
+echo "rpi-linux Git rpi-3.6.y @ $(get_kernel_version)" >> "$BUILD_INFO"
 
 cd ..
 
