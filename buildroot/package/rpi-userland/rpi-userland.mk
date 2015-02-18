4330ae91c3bfa4e7bfc20f90d37e167f25d9ff4c
Andrew Scheller <github@loowis.durge.org>
2015-02-18 16:15:30 +0000

    Bump firmware, kernel and userland versions

--- buildroot/package/rpi-userland/rpi-userland.mk
+++ buildroot/package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = 3b81b91c18ff19f97033e146a9f3262ca631f0e9
+RPI_USERLAND_VERSION = c5341f09dc686c17966369485f7dd27f58dc081c
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3c
 RPI_USERLAND_LICENSE_FILES = LICENCE
