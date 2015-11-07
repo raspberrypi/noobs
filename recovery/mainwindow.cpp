78b8e4dc6395e1a4a183995805468529b12a3a9e
Floris Bos <bos@je-eigen-domein.nl>
2015-11-07 20:03:27 +0100

    Enable SSL support
    - Enable SSL support in Qt
    - Enable installation of CA certificates
    - Set system clock to the date/time of the repository web server ("Date" HTTP response header)
    
    Closes #266

--- recovery/mainwindow.cpp
+++ recovery/mainwindow.cpp
@@ -1057,6 +1057,22 @@ void MainWindow::downloadListComplete()
     QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
     int httpstatuscode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
 
+    /* Set our clock to server time if we currently have a date before 2015 */
+    QByteArray dateStr = reply->rawHeader("Date");
+    if (!dateStr.isEmpty() && QDate::currentDate().year() < 2015)
+    {
+        // Qt 4 does not have a standard function for parsing the Date header, but it does
+        // have one for parsing a Last-Modified header that uses the same date/time format, so just use that
+        QNetworkRequest dummyReq;
+        dummyReq.setRawHeader("Last-Modified", dateStr);
+        QDateTime parsedDate = dummyReq.header(dummyReq.LastModifiedHeader).toDateTime();
+
+        struct timeval tv;
+        tv.tv_sec = parsedDate.toTime_t();
+        tv.tv_usec = 0;
+        settimeofday(&tv, NULL);
+    }
+
     if (reply->error() != reply->NoError || httpstatuscode < 200 || httpstatuscode > 399)
     {
         if (_qpd)
