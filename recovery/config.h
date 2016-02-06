b428eeebe16e7ddd7d0854fe51e10657372c216e
NicoHood <NicoHood@users.noreply.github.com>
2016-02-06 09:06:13 +0100

    Fixed version number to 1.5

--- recovery/config.h
+++ recovery/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 /* Version number displayed in the title bar */
-#define VERSION_NUMBER "1.4"
+#define VERSION_NUMBER "1.5"
 
 /* Color of the background */
 // #define BACKGROUND_COLOR  Qt::white
