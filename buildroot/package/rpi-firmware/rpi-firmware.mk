c40088377dfacafdb805f634ad3d3d4744582e0a
matthuisman <matthuisman@users.noreply.github.com>
2016-03-11 14:36:47 +1300

    Add bcm2710-rpi-3-b.dtb (fixes WiFi support for Raspberry Pi 3)

--- buildroot/package/rpi-firmware/rpi-firmware.mk
+++ buildroot/package/rpi-firmware/rpi-firmware.mk
@@ -18,6 +18,7 @@ define RPI_FIRMWARE_INSTALL_DTB
 	$(INSTALL) -D -m 0644 $(@D)/boot/bcm2708-rpi-b.dtb $(BINARIES_DIR)/rpi-firmware/bcm2708-rpi-b.dtb
 	$(INSTALL) -D -m 0644 $(@D)/boot/bcm2708-rpi-b-plus.dtb $(BINARIES_DIR)/rpi-firmware/bcm2708-rpi-b-plus.dtb
 	$(INSTALL) -D -m 0644 $(@D)/boot/bcm2709-rpi-2-b.dtb $(BINARIES_DIR)/rpi-firmware/bcm2709-rpi-2-b.dtb
+	$(INSTALL) -D -m 0644 $(@D)/boot/bcm2710-rpi-3-b.dtb $(BINARIES_DIR)/rpi-firmware/bcm2710-rpi-3-b.dtb
 endef
 endif
 
