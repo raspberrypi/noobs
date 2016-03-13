3b0c413a82f0c35410c109abefbebfa2ae9d5f3f
fonic <fonic.maxxim@live.com>
2016-03-13 21:58:40 +0100

    Timeout 5 seconds, background black

--- recovery/config.h
+++ recovery/config.h
@@ -5,11 +5,10 @@
 #define VERSION_NUMBER "1.7"
 
 /* Countdown for OS selection dialog in seconds */
-#define OS_SELECTION_TIMEOUT 10
+#define OS_SELECTION_TIMEOUT 5
 
 /* Color of the background */
-// #define BACKGROUND_COLOR  Qt::white
-#define BACKGROUND_COLOR  QColor(0xde, 0xde, 0xde)
+#define BACKGROUND_COLOR  QColor(0x00, 0x00, 0x00)
 
 /* Highlight color of installed OS */
 #define INSTALLED_OS_BACKGROUND_COLOR  QColor(0xef,0xff,0xef)
