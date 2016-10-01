541f47ef25850311be20d104c063c54874f3dcd5
Serge Schneider <serge@raspberrypi.org>
2016-10-01 17:44:24 +0100

    Change NOOBS version to 2.0
    Bump firmware and kernel versions
    
    Copy *.dtbo instead of *.dtb files
    
    Enable CONFIG_DMA_BCM2835
    
    Run kernel config through savedefconfig

--- buildroot/package/rpi-firmware/rpi-firmware.mk
+++ buildroot/package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 951799bbcd795ddf27769d14acf4813fdcbe53dc
+RPI_FIRMWARE_VERSION = ec63df146f454e8cab7080380f9138246d877013
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
@@ -24,7 +24,7 @@ endif
 
 ifeq ($(BR2_PACKAGE_RPI_FIRMWARE_INSTALL_DTB_OVERLAYS),y)
 define RPI_FIRMWARE_INSTALL_DTB_OVERLAYS
-	for ovldtb in  $(@D)/boot/overlays/*.dtb; do \
+	for ovldtb in  $(@D)/boot/overlays/*.dtbo; do \
 		$(INSTALL) -D -m 0644 $${ovldtb} $(BINARIES_DIR)/rpi-firmware/overlays/$${ovldtb##*/} || exit 1; \
 	done
 endef
