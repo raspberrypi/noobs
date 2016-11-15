25397d61dd29297afbc168a53fca3d2011d0b5fe
Serge Schneider <serge@raspberrypi.org>
2016-11-15 15:10:14 +0000

    Bump version to 2.1

--- recovery/config.h
+++ recovery/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 /* Version number displayed in the title bar */
-#define VERSION_NUMBER "2.0"
+#define VERSION_NUMBER "2.1"
 
 /* Color of the background */
 // #define BACKGROUND_COLOR  Qt::white
