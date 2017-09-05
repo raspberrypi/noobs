1a48956d4a49a540eeb7a0e87365fec8becff7d2
SnooeyNET(Yoon, Seongsu) <sople1@snooey.net>
2017-09-05 17:04:54 +0900

    change Arora version for Korean user

--- recovery/mainwindow.cpp
+++ recovery/mainwindow.cpp
@@ -934,7 +934,7 @@ void MainWindow::startBrowser()
         return;
     QProcess *proc = new QProcess(this);
     QString lang = LanguageDialog::instance("en", "gb")->currentLanguage();
-    if (lang == "gb" || lang == "us" || lang == "ko" || lang == "")
+    if (lang == "gb" || lang == "us" || lang == "")
         lang = "en";
     proc->start("arora -lang "+lang+" "+HOMEPAGE);
 }
