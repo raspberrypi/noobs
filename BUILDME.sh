b9614a452d88c1121e3ac7a189a220515a9e5654
Serge Schneider <serge@raspberrypi.org>
2018-04-18 08:49:04 +0100

    Bump kernel to 4.14.34

--- BUILDME.sh
+++ BUILDME.sh
@@ -124,7 +124,7 @@ for i in $*; do
 
     # Update raspberrypi/linux rpi-4.1.y HEAD version in buildroot/.config to latest
     if [ $i = "update-kernel" ]; then
-        update_github_kernel_version raspberrypi/linux rpi-4.4.y
+        update_github_kernel_version raspberrypi/linux rpi-4.14.y
     fi
 
     # Option to build just recovery without completely rebuilding both kernels
@@ -181,7 +181,7 @@ echo "NOOBS Version: $(sed -n 's|.*VERSION_NUMBER.*\"\(.*\)\"|v\1|p' ../recovery
 echo "NOOBS Git HEAD @ $(git rev-parse --verify HEAD)" >> "$BUILD_INFO"
 echo "rpi-userland Git master @ $(get_package_version rpi-userland)" >> "$BUILD_INFO"
 echo "rpi-firmware Git master @ $(get_package_version rpi-firmware)" >> "$BUILD_INFO"
-echo "rpi-linux Git rpi-4.9.y @ $(get_kernel_version)" >> "$BUILD_INFO"
+echo "rpi-linux Git rpi-4.14.y @ $(get_kernel_version)" >> "$BUILD_INFO"
 
 cd ..
 
