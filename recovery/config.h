6a33a6e072490a76c9a187ba64f51092ffb7c2d6
Michael James <mike@mjjames.co.uk>
2019-06-11 19:23:23 +0100

    reverted version number change as part of PR review

--- recovery/config.h
+++ recovery/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 /* Version number displayed in the title bar */
-#define VERSION_NUMBER "3.1"
+#define VERSION_NUMBER "3.0"
 
 /* Color of the background */
 // #define BACKGROUND_COLOR  Qt::white
