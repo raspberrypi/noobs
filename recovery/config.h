5b11887d5e8da1f8d6138176a3f9281b658a4972
Serge Schneider <serge@raspberrypi.org>
2020-02-04 14:42:56 +0000

    Bump version to 3.3

--- recovery/config.h
+++ recovery/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 /* Version number displayed in the title bar */
-#define VERSION_NUMBER "3.2"
+#define VERSION_NUMBER "3.3"
 
 /* Color of the background */
 // #define BACKGROUND_COLOR  Qt::white
